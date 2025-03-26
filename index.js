cbc900e45c60d4027fe452a9688ecad6b4361e7c
Yhasmin-Barros <yhasminbarros1@gmail.com>
2025-03-26 17:51:40 +0000

    Aula 26/03/2025

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
-app.get ('/', (request, response) =>{
+app.get ('/', (request, response) =>{ //response = objeto de resposta (usado para devolver para o usuário),
+//  request = objeto de requisição (do que veio para o servidor)//
+
     response.send ('Olá Codespace!!!');
 });
 
-app.get('/mensagem', (request, response) => {
-    response.send('Boa Tarde!')
-})
+app.get('/mensagem/:nome', (request, response) => {
+    response.send('Boa Tarde! ' + request.params['nome']);
+});
 
 app.listen(port, () => {
     console.log("Aplicação em execução na porta " + port);
